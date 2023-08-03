53e331489fb132cb496521c3a3af280918208ebe
reddysomula6789@gmail.com <reddysomula6789@gmail.com>
2023-08-03 16:27:45 +0530

    ...

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "7y89eo",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+
     },
   },
 });
